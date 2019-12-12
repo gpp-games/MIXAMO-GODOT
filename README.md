b4d306c8c2467f7065eb96521094823811837fb4
gppg <gpp-games@protonmail.com>
2019-12-12 17:37:10 +0100

    Cleaned up a bit. Now you don't have to use empty .blend's anymore. Updated README too.

--- README.md
+++ README.md
@@ -1,15 +1,34 @@
 # MIXAMO-GODOT
-Blender-Script, that lets you batch import + export Mixamo-Animations from .fbx to .escn . With and without RootMotion.
-! Blender Version 2.80, 2.81 - Not tested with other versions.
-! Doesn't work with meshes currently, only bare armatures with animations.
-! Can't join animations in a single file, yet.
-! Only work's with .fbx
+- Blender Version 2.80, 2.81 - Not tested with other versions.
+- Batch-Imports Mixamo-Animations without (!) skin from Fbx.
+- Renames the actions according to Fbx-Filename.
+- Can Batch-Save individual animations as Blendfiles.
+- Also can Batch-Export animations as ESCN-Files,
+- using the Godot-Blender-Exportes (Has to be installed).
+- Doesn't remove the 'MixamoRig:' prefix from the Bone-Names, yet.
+- Can't yet pack animations in a single .blend/.escn
+- Doesn't work with meshes currently, only bare armatures with animations.
+- Can't join animations in a single file, yet.
+- Only work's with Fbx-Files.
+- X, Z locations get removed completely from the Hip-Bone (!)
 
 How to use:
-1. In the folder with your Mixamo-Animations (.fbx) save a .blend. File 
-1b. A-Select and X-Remove (default cube etc.) !
-2. With the saved .blend open, load the script in the text-editor.
-2b. Change The Option-Variables for what you need.
-3. run the script
-4. Done!
+0. You might want to replace spaces in the filenames (optional)
+1. To see printed warnings/errors logs, open Blender in Terminal
+2. Open a new file in blender and save it in the directory, with
+   the files you downloaded from Mixamo. Working-Directory.
+   If the Blendfile contains objects (default-cube or a model, camera etc.),
+   the script will backup the file.
+2. Open the script in the Text-Editor.
+3. Change the Option-Variables True/False, for what you need.
+4. Run the Script, done.
 
+When you've exported to .escn you can:
+
+$ cd your-anim-folder/escn_out
+$ godot -e
+
+______________________________________________________________
+
+replacing ' ' with '_' on linux-terminal:
+for foo in *.fbx; do mv "$foo" `echo $foo | tr ' ' '_'` ; done
--- mixamo_godot.py
+++ mixamo_godot.py
@@ -9,17 +9,17 @@ context = bpy.context
 ops = bpy.ops
 dat = bpy.data
 
-# Options: Change the flags in Main()
+# Option Variables:
 EXPORT_ESCN = True
-SAVE_INDIVIDUAL_BLENDS = True # Don't touch this. Has to be True right now.
-SAVE_AS_ANIMATION_LIB = False
+SAVE_INDIVIDUAL_BLENDS = True
+SAVE_AS_ANIMATION_LIB = False # Not implemented, yet.
 ADD_ROOT_MOTION = True
 
 # Script Variables:
 filepath = bpy.data.filepath
 ESCN_OUT_DIR_NAME =  'escn_out'
 
-# Armature and it'sTransforms
+# Armature Variables:
 ARMATURE_NAME = 'Armature'
 armature = None
 init_arma_scale = (0, 0, 0)
@@ -29,6 +29,7 @@ init_arma_rotation = (0, 0, 0)
 RM_BONE_NAME = 'RootMotion'
 RM_BONE_SIZE = 0.3
 HIP_BONE_NAME = 'Hips'
+
 
 ### Script Functions ###
 def importFBX(filePath, filename):
@@ -70,117 +71,123 @@ def scale_keyframes(scalar):
 def saveBlendfile(filePath, filename):
     bpy.ops.wm.save_mainfile(filepath = filePath + "/" + filename + ".blend")
 
-##############################
-### MAIN Function #########
-########################
-def Main():
-    if filepath is '':
-        print('Save .blend in working directory first!')
-    else:
-        # For file in Working-Dir,
-        for filename in os.listdir(os.path.dirname(filepath)):
-            # if is FBX-File:
-            if filename.endswith(".fbx"):
-                # Import and Prepare
-                importFBX(os.path.dirname(filepath), filename)
-                armature = bpy.data.objects[ARMATURE_NAME]
-                init_arma_scale = (context.object.scale[0], context.object.scale[1],
-                                    context.object.scale[2])
 
-                # Optimize for Godot
+## Main:
+if filepath is '':
+    print('Save .blend in working directory first!')
+else:
+    if len(dat.objects) > 0:
+        bpy.ops.wm.save_mainfile(filepath = filepath + ".bak")
+        for obj in dat.objects:
+            dat.objects.remove(obj)
+        bpy.ops.wm.save_mainfile(filepath = filepath)
+        print("backed up $filepath")
+
+    # For file in Working-Dir,
+    for filename in os.listdir(os.path.dirname(filepath)):
+        # if is FBX-File:
+        if filename.endswith(".fbx"):
+            # Import and Prepare
+            importFBX(os.path.dirname(filepath), filename)
+            armature = bpy.data.objects[ARMATURE_NAME]
+            init_arma_scale = (context.object.scale[0], context.object.scale[1],
+                                context.object.scale[2])
+
+            # Optimize for Godot
+            selectArmature(armature)
+            ops.object.transform_apply(location=True, rotation=True, scale=True)
+            ops.object.mode_set(mode = 'POSE', toggle = False)
+            selectAllPoseBones(armature, False)
+            armature.pose.bones[HIP_BONE_NAME].bone.select = True
+            # Rename Action
+            dat.actions[0].name = str(os.path.splitext(filename)[0])
+            scale_keyframes(init_arma_scale[0])
+
+            # Add Root Motion
+            if ADD_ROOT_MOTION == True:
+                loc_fcurves = { 0 : 'x', 1 : 'z' }
+                rm_bone = None
+                context.area.type = 'VIEW_3D'
+                context.scene.tool_settings.transform_pivot_point = 'CURSOR'
+
+                # Add the RootMotion-Bone:
+                ops.object.mode_set(mode = 'OBJECT', toggle = False)
                 selectArmature(armature)
-                ops.object.transform_apply(location=True, rotation=True, scale=True)
-                ops.object.mode_set(mode = 'POSE', toggle = False)
-                selectAllPoseBones(armature, False)
-                armature.pose.bones[HIP_BONE_NAME].bone.select = True
-                # Rename Action
-                dat.actions[0].name = str(os.path.splitext(filename)[0])
-                scale_keyframes(init_arma_scale[0])
+                ops.object.mode_set(mode = 'EDIT', toggle = False)
+                ops.armature.bone_primitive_add(name = RM_BONE_NAME)
 
-                # Add Root Motion
-                if ADD_ROOT_MOTION == True:
-                    loc_fcurves = { 0 : 'x', 1 : 'z' }
-                    rm_bone = None
-                    context.area.type = 'VIEW_3D'
-                    context.scene.tool_settings.transform_pivot_point = 'CURSOR'
+                # Push down the RootMotion bone to .3 scale and store in variable
+                for bone in armature.data.edit_bones:
+                    if fnmatch.fnmatchcase(bone.name, RM_BONE_NAME):
+                        print(bone)
+                        rm_bone = bone
+                        bone.head = (0.0, 0.0, 0.0)
+                        bone.tail = (0.0, 0.0, 0.3)
 
-                    # Add the RootMotion-Bone:
-                    ops.object.mode_set(mode = 'OBJECT', toggle = False)
-                    selectArmature(armature)
-                    ops.object.mode_set(mode = 'EDIT', toggle = False)
-                    ops.armature.bone_primitive_add(name = RM_BONE_NAME)
+                # Parent the Hip- to RootMotion-Bone
+                for bone in armature.data.edit_bones:
+                    if fnmatch.fnmatchcase(bone.name, HIP_BONE_NAME):
+                        bone.parent = rm_bone
 
-                    # Push down the RootMotion bone to .3 scale and store in variable
-                    for bone in armature.data.edit_bones:
-                        if fnmatch.fnmatchcase(bone.name, RM_BONE_NAME):
-                            print(bone)
-                            rm_bone = bone
-                            bone.head = (0.0, 0.0, 0.0)
-                            bone.tail = (0.0, 0.0, 0.3)
-
-                    # Parent the Hip- to RootMotion-Bone
-                    for bone in armature.data.edit_bones:
-                        if fnmatch.fnmatchcase(bone.name, HIP_BONE_NAME):
-                            bone.parent = rm_bone
-
-                    # Copy the X and Y Location F-Curves from Hip-Bone,
-                    # on RootMotion-Bone add a Location-Keyframe on Frame 1 and paste the F-Curves:
-                    for loc in loc_fcurves:
-                        ops.object.mode_set(mode = 'POSE', toggle = False)
-                        selectAllPoseBones(armature, False)
-                        selectPoseBone(armature, HIP_BONE_NAME)
-                        context.area.type = 'GRAPH_EDITOR'
-                        context.space_data.pivot_point = 'CURSOR'
-                        context.scene.frame_current = 1
-                        context.space_data.cursor_position_y = 0
-                        context.space_data.dopesheet.filter_text = loc_fcurves[loc] + " location"
-                        ops.graph.select_all(action = 'SELECT')
-                        ops.graph.copy()
-                        selectAllPoseBones(armature, False)
-                        selectPoseBone(armature, RM_BONE_NAME)
-                        ops.anim.keyframe_insert_menu(type='Location')
-                        context.space_data.dopesheet.filter_text = loc_fcurves[loc] + " location"
-                        ops.graph.paste()
-
-                    # Delete the Y-Location F-Curve from the RootMotion-Bone:
-                    context.space_data.dopesheet.filter_text = "y location"
-                    ops.graph.select_all(action = 'SELECT')
-                    ops.graph.delete()
-
-                    # Delete X and Z Location from the Hip-Bone
+                # Copy the X and Y Location F-Curves from Hip-Bone,
+                # on RootMotion-Bone add a Location-Keyframe on Frame 1 and paste the F-Curves:
+                for loc in loc_fcurves:
+                    ops.object.mode_set(mode = 'POSE', toggle = False)
                     selectAllPoseBones(armature, False)
                     selectPoseBone(armature, HIP_BONE_NAME)
-                    for loc in loc_fcurves:
-                        context.space_data.dopesheet.filter_text = loc_fcurves[loc] + " location"
-                        ops.graph.delete()
+                    context.area.type = 'GRAPH_EDITOR'
+                    context.space_data.pivot_point = 'CURSOR'
+                    context.scene.frame_current = 1
+                    context.space_data.cursor_position_y = 0
+                    context.space_data.dopesheet.filter_text = loc_fcurves[loc] + " location"
+                    ops.graph.select_all(action = 'SELECT')
+                    ops.graph.copy()
+                    selectAllPoseBones(armature, False)
+                    selectPoseBone(armature, RM_BONE_NAME)
+                    ops.anim.keyframe_insert_menu(type='Location')
+                    context.space_data.dopesheet.filter_text = loc_fcurves[loc] + " location"
+                    ops.graph.paste()
+
+                # Delete the Y-Location F-Curve from the RootMotion-Bone:
+                context.space_data.dopesheet.filter_text = "y location"
+                ops.graph.select_all(action = 'SELECT')
+                ops.graph.delete()
+
+                # Delete X and Z Location from the Hip-Bone
+                selectAllPoseBones(armature, False)
+                selectPoseBone(armature, HIP_BONE_NAME)
+                for loc in loc_fcurves:
+                    context.space_data.dopesheet.filter_text = loc_fcurves[loc] + " location"
+                    ops.graph.delete()
+
+                print("RootMotion added!")
 
 
 
-                # Save Armatures/Animations as individual Blendfiles
-                if SAVE_INDIVIDUAL_BLENDS == True:
-                    saveBlendfile(os.path.dirname(filepath), str(os.path.splitext(filename)[0]))
-                    print("saved .blend!")
+            # Save Armatures/Animations as individual Blendfiles
+            if SAVE_INDIVIDUAL_BLENDS == True:
+                saveBlendfile(os.path.dirname(filepath), str(os.path.splitext(filename)[0]))
+                print("saved .blend!")
 
-                # Export to Godot
-                if EXPORT_ESCN == True:
-                    if not os.path.isdir(str(os.path.dirname(filepath)) + '/' + ESCN_OUT_DIR_NAME):
-                        os.mkdir(str(os.path.dirname(filepath)) + '/' + ESCN_OUT_DIR_NAME)
-                        prj_godot = open(str(os.path.dirname(filepath))
-                                            + '/' + ESCN_OUT_DIR_NAME + '/' + 'project.godot', 'w')
-                    io_scene_godot.export(str(os.path.dirname(filepath))
-                                            + '/' + ESCN_OUT_DIR_NAME + '/'
-                                            + str(os.path.splitext(filename)[0]) + '.escn')
+            # Export to Godot
+            if EXPORT_ESCN == True:
+                if not os.path.isdir(str(os.path.dirname(filepath)) + '/' + ESCN_OUT_DIR_NAME):
+                    os.mkdir(str(os.path.dirname(filepath)) + '/' + ESCN_OUT_DIR_NAME)
+                    prj_godot = open(str(os.path.dirname(filepath))
+                                + '/' + ESCN_OUT_DIR_NAME + '/' + 'project.godot', 'w')
 
-                # Delete all Objects
-                ops.object.mode_set(mode = 'OBJECT', toggle = False)
-                ops.object.select_all(action = 'SELECT')
-                ops.object.delete()
-                # Remove all Armatures
-                for arma in dat.armatures:
-                    dat.armatures.remove(arma)
-                # Remove all Actions
-                for a in dat.actions:
-                    dat.actions.remove(a)
+                io_scene_godot.export(str(os.path.dirname(filepath))
+                                    + '/' + ESCN_OUT_DIR_NAME + '/'
+                                    + str(os.path.splitext(filename)[0]) + '.escn')
+                print("exported .escn!")
 
-
-Main()
+            # Delete all Objects
+            ops.object.mode_set(mode = 'OBJECT', toggle = False)
+            ops.object.select_all(action = 'SELECT')
+            ops.object.delete()
+            # Remove all Armatures
+            for arma in dat.armatures:
+                dat.armatures.remove(arma)
+            # Remove all Actions
+            for a in dat.actions:
+                dat.actions.remove(a)
